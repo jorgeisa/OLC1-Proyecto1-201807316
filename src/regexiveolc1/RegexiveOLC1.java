cdc2a220b5aeb073747888406dc7d2ce04a50051
Jorge Isaac Xicol Vicente <isaacxicol@gmail.com>
2021-02-26 16:29:32 -0600

    Lexico, sintactico e Interfaz

---
+++ src/regexiveolc1/RegexiveOLC1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regexiveolc1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.StringReader;
+/**
+ *
+ * @author Isaac
+ */
+public class RegexiveOLC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola esto es regexive");
+        interpretar("entrada.txt");
+    }
+    
+    private static void interpretar(String path) {
+        Analizador.Sintactico parse;
+        try {
+            parse = new Analizador.Sintactico(new Analizador.Lexico(new BufferedReader(new FileReader(path))));
+            parse.parse();        
+        } catch (Exception ex) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+ex.getCause());
+        } 
+    }
+}
